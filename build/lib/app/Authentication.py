07033f1434f4863b136ca59fb4b9fcb905d0c4ae
sthumm23 <58864629+sthumm23@users.noreply.github.com>
2020-07-20 23:16:08 -0500

    commit app files

---
+++ build/lib/app/Authentication.py
@@ -0,0 +1,26 @@
+from User_db import Users_DB
+class User():
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
+
+def authenticate(username, password):
+    user_DB = Users_DB(username, password)
+    user_details = user_DB.findUsername()
+    return User(user_details[0], user_details[1], user_details[2])
+
+def identity(payload):
+    user_DB = Users_DB(username = "", password = "")
+    user_id = payload['identity']
+    user_details = user_DB.findID(user_id)
+    return User(user_details[0], user_details[1], user_details[2])
+        
+
+        
+    
+    
+
+
+    
+
---
+++ build/lib/app/COVID_Flask.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+from flask_jwt import JWT, jwt_required
+from flask_restful import Resource, Api
+from COVID_Tracker_Data import COVID_Data
+from Authentication import authenticate, identity
+from User_db import Users_DB
+import db
+
+
+app = Flask(__name__)
+app.secret_key = db.jwt_cfg['secret_key']
+
+jwt = JWT(app, authenticate, identity)
+
+@app.route('/covid', methods = ['POST'])
+@jwt_required()
+def covid_data():
+    try:
+        state = request.form.get('state')
+        COVID_Tracker = COVID_Data(state)
+        stateInfo,status = COVID_Tracker.get_StateData()
+        if status !=200:
+            return {"message":"No data returned, Probably invalid state:"+ state}, status
+        state_data = {}
+
+        state_data['Case Growth Rate'] = COVID_Tracker.getCaseGrowthRate()
+        state_data['Mortality Rate'] = COVID_Tracker.getMortalityRate()
+        state_data['Hospitalizaiton Rate'] = COVID_Tracker.getTotalHospitalizations()
+        state_data['Recovery Rate'] = COVID_Tracker.getRecoveryRate()
+        state_data['Advisory'] = COVID_Tracker.TravelAdvisory()   
+        return state_data , 200
+    except Exception as exp:
+        return {"message":str(exp)}, 500
+
+@app.route('/register', methods = ['POST'])
+def register_user():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    user_DB = Users_DB(username, password)
+    user_DB.register()
+    return "Register Was Successful"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
